71fd6070e9f80d4ce0d573a44090fdc5b2dca7d1
Rokas Stasiulis <rokastas@gmail.com>
2024-01-23 15:53:06 +0100

    functional part done

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get "tasks", to: "tasks#index"
-  get "tasks/:id", to: "tasks#show", as: :show
+  get "tasks/:id", to: "tasks#show", as: :task
+  get "new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#destroy"
 end
